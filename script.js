1f08176e73ab1a6029254d559c506ea20bcb92c8
Haidar Khalid <110727504+HaidarKhalid@users.noreply.github.com>
2022-12-02 15:51:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+let inpEl = document.querySelector(".text-inp");
+let addEl = document.querySelector(".add-btn")
+let ulEl = document.querySelector(".ul-el")
+let added = 0
+
+addEl.addEventListener("click", ()=>{
+    add()
+})
+
+inpEl.addEventListener("keydown", (e)=> {
+    if (e.key == "Enter") {
+        add()
+    } else {
+        return
+    }
+})
+
+
+
+function add() {
+
+  inpEl = document.querySelector(".text-inp")
+
+  if (inpEl.value == "" || inpEl.value == " " || inpEl.value == "  "|| inpEl.value == "   "|| inpEl.value == "    ") {
+    return 
+} else {
+    added++
+    ulEl.innerHTML += `
+    <br class="br${added}">
+    <button onclick="del(${added})" class="del-btn btn${added}">x</button>  
+    <li onclick="done(${added})" class="li-el li${added}">${inpEl.value}</li>
+    `
+//<input type="checkbox" onclick="done(${added})" class="check${added}">
+    inpEl.value = ""
+  }
+}
+
+function del(num) {
+    ulEl.removeChild(document.querySelector(`.btn${num}`))
+    ulEl.removeChild(document.querySelector(`.li${num}`))
+    ulEl.removeChild(document.querySelector(`.br${num}`))
+  //  ulEl.removeChild(document.querySelector(`.check${num}`))
+}
+
+function done(val) {
+    document.querySelector(`.li${val}`).classList.toggle("active")
+}
